34eaf71a35f8473b93ec724d364b86f15bf8e532
Fourquet <Daniel.Fourquet@vdot.virginia.gov>
2023-01-12 11:03:22 -0500

    Start xd_to_rns

---
+++ xd_to_rns.py
@@ -0,0 +1,38 @@
+
+
+
+inputLRS = r''
+inputXD = r''
+
+
+class XDSegment:
+    
+
+
+def get_begin_middle_end(geom):
+    """ Returns the begin, middle, and end points of the input geometry """
+
+    return
+
+
+def find_nearby_routes(point, lrs=inputLRS, searchDistance="100 METERS"):
+    """ Given an input point, will return a list of all routes within the searchDistance """
+
+    return
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
+
+    # Get list of XDSegID values
+
+    # For XD in XDs:
+        # Get XD Geometry
+        # Get 
+        # First_Iteration - First we find the nearby routes for the begin, middle, and end point of the XD segment.
+            #beginPoint, midPoint, endPoint = get_begin_middle_end(geom)
